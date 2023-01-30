694a1f5342f7cc5feedeb2375ddd6e84a797ef32
MartinGadea12 <martiingadeea1996@gmail.com>
2023-01-30 19:34:47 -0300

    Modificaciones finales

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import styles from "../Loader/Loader.module.css";
+
+const Loader = ()  => {
+
+    return (
+        <div className={styles.loader}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+    </div>
+    );
+  }
+  
+  export default Loader;
+  
